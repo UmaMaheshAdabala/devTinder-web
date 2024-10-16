d917176a28225affde412a03dfbd916c659db082
Uma Mahesh <adabala.um@gmail.com>
2024-10-16 15:01:44 +0530

    bug: Reload-Login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      } else {
+        console.error(err);
+      }
+    }
+  };
+  useEffect(() => {
+    if (!user) {
+      fetchUser();
+    }
+  }, []);
   return (
     <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
